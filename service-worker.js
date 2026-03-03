0164c7996bb59b8fbae311d4fd4a3a3e62ecac9d
wak4kusa296 <162152488+wak4kusa296@users.noreply.github.com>
2026-03-04 08:00:07 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = "dfle-v1";
+const ASSETS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./price_table.csv",
+  "./DFLE.png",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((response) => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        return response;
+      });
+    })
+  );
+});
